b10dde892648d7abd7826a325acbcd854d8b64d3
SriramDhanabalan <145642115+SriramDhanabalan@users.noreply.github.com>
2023-09-29 13:28:48 +0530

    JavaTask

---
+++ Task1.java
@@ -0,0 +1,63 @@
+package com.ebrain.task1;
+
+public class Task1 {
+	
+	public static void main(String[] args) {
+		int a= 6, b=8, c=10;
+		
+	
+		System.out.println(a+b);
+		
+		System.out.println(a-b);
+
+	    System.out.println(a*b);
+	    
+	    System.out.println(b/c);
+	    
+	    float D= b/c;
+	    
+	    System.out.println(D);
+	    
+	    float E = (float)b / (float) c;
+	    
+	    System.out.println(E);
+	 
+	    System.out.println(a%c);
+	    
+	    System.out.println(a<c);
+        	
+	    System.out.println(b>c);
+	
+	    System.out.println(a==b);
+	    
+	    System.out.println(b!=c);
+	    
+	    boolean A = true; 
+	    boolean B = false;
+	    boolean C = true;
+	  	    
+	    System.out.println(!A);
+	    System.out.println(!B);
+	  
+	    System.out.println(A||B); 
+	    System.out.println(A||C); 
+	  	    	  
+	    System.out.println(A&&B);
+	    System.out.println(A&&C);
+	  
+	   
+	    if (a>b) {
+	    	
+	    	System.out.println("Positive");
+	    }
+	       else {
+	    		System.out.println("Negative");
+	    	}
+	   
+	    
+	
+	
+	
+	}
+
+}
